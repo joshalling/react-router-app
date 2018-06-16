7c0238ab3d7de4d75446f8841ab86a3001b007ed
Josh Alling <jalling90@gmail.com>
2018-06-16 09:17:37 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-router-app
+
+A small app that explores the basics of the react-router module.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">React Router App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
